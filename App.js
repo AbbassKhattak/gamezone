72572d0359c78f6fa4d1dea38334559adac4d892
AbbasAli <abbass.khattak@gmail.com>
2023-03-23 12:24:38 +0500

    after adding custom fonts

--- App.js
+++ App.js
@@ -1,23 +1,43 @@
 import React,{useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as Font from 'expo-font';
 import Home from './screens/home';
 
+import { AppLoading } from 'expo';
+
+
+const getFonts =() => {
+  return Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  });
+}
+
 export default function App() {
-  
+
+  const [fontsLoaded,setFontsLoaded] = useState(false);
+  if(fontsLoaded){
     return (
-      <View style={styles.container}>
-        <Home />
-      </View>
+      <Home/>
     );
   }
- 
+ else{
+  return(
+    <AppLoading 
+       startAsync= {getFonts}
+      onFinish={()=> setFontsLoaded(true)}
+    />
+ );
+}
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff'
   },
- 
-  
+  text:{
+    fontFamily:'nunito-bold'
+  }
 });
