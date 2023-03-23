1c864cbc11c37939e6df9d1079cf4a12da8406ea
AbbasAli <abbass.khattak@gmail.com>
2023-03-23 12:08:58 +0500

    code before adding custom fonts

---
+++ screens/reviewDetails.js
@@ -0,0 +1,19 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function ReviewDetails() {
+  return (
+    <View style={styles.container}>
+      <Text>ReviewDetails</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
